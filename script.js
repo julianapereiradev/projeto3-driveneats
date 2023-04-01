c170a9e8df5286d91e77357887e61a5847529568
juliana <julianapereira.dev@gmail.com>
2023-03-31 23:12:56 -0300

    Select agora com o ícone de check junto

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 function selecionarPrato(seletor) {
-    //primeiro vou verificar se existe botao ja selecionado
-    //buscar o botao com a classe selecionada
     const botaoSelecionadoAnteriormente = document.querySelector('.row-caixas-pratos .selecionado')
-    console.log(botaoSelecionadoAnteriormente)
-    //se existir botao selecionado,
+   
     if (botaoSelecionadoAnteriormente !== null) {
-        //remove a classe selecionado desse botao
         botaoSelecionadoAnteriormente.classList.remove('selecionado')
     }
-    //buscar o novo botao clicado
+    
     const botao = document.querySelector(seletor)
-
-    //adiciona a classe selecionado
     botao.classList.add('selecionado')
 }
 
 function selecionarBebida(seletor) {
-    //primeiro vou verificar se existe botao ja selecionado
-    //buscar o botao com a classe selecionada
+   
     const botaoSelecionadoAnteriormente = document.querySelector('.row-caixas-bebidas .selecionado')
-    console.log(botaoSelecionadoAnteriormente)
-    //se existir botao selecionado,
+   
     if (botaoSelecionadoAnteriormente !== null) {
-        //remove a classe selecionado desse botao
         botaoSelecionadoAnteriormente.classList.remove('selecionado')
     }
-    //buscar o novo botao clicado
-    const botao = document.querySelector(seletor)
-
-    //adiciona a classe selecionado
+  
+    const botao = document.querySelector(seletor) 
     botao.classList.add('selecionado')
 }
 
